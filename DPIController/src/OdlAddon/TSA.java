72d9b916b4d37cd12773af74e59539883915b2a0
orenstal <orenstal@gmail.com>
2015-03-28 15:15:42 +0300

    adding OpenDayLight addon

---
+++ DPIController/src/OdlAddon/TSA.java
@@ -0,0 +1,23 @@
+package OdlAddon;
+
+import java.util.LinkedList;
+
+public class TSA {
+
+	public static void main(String[] args) {
+		System.out.println("TSA !!");
+	}
+
+	// this method receives source and destination host ids, and returns all the paths between them.
+	// remember that the policy chain includes only middleboxes (hosts) and not the switches and routers,
+	// so i need to find all the paths between two hosts.
+	public LinkedList<LinkedList<Link>> getAllPathsBetweenHosts(String srcHostId, String dstHostId) {
+		return null;
+	}
+
+
+
+
+
+
+}
